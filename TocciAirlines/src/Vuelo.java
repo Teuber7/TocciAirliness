cff9fdcfb776734eae22eb60faf30dafbbd432fa
gsaguati <gsaguati@gmail.com>
2024-09-24 10:22:43 -0300

    g

--- TocciAirlines/src/Vuelo.java
+++ TocciAirlines/src/Vuelo.java
@@ -1,4 +1,23 @@
-
 public class Vuelo {
 
+    private String destino;
+    private double precio;
+
+    public Vuelo(String destino, double precio) {
+        this.destino = destino;
+        this.precio = precio;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    @Override
+    public String toString() {
+        return destino + " - $" + precio;
+    }
 }
